50766c3bb8a0653f0a9bac5a054685361bc1bb4e
Nicky <nmcollano@gmail.com>
2023-05-02 22:58:29 +0800

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,10 @@ function App() {
       </GridItem>
 
       <Show above="lg">
-        <GridItem area="aside" bg="#dedede">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main" bg="coral">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
