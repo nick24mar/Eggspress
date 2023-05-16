335d1e43812addd47d95467825fbd0b6ec8d5332
Nicky <nmcollano@gmail.com>
2023-05-16 23:18:36 +0800

    Buil game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,10 +1,9 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 import TextExpandable from "../components/TextExpandable";
 import useGame from "../hooks/useGame";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
-import GameAttributes from "../components/GameAttributes";
 
 function GameDetailPage() {
   const { slug } = useParams();
@@ -12,13 +11,14 @@ function GameDetailPage() {
 
   if (isLoading) return <Spinner />;
 
-  if (error) throw error;
+  if (error || !game) throw error;
 
   return (
     <>
-      <Heading mb={3}>{game?.name}</Heading>
-      <TextExpandable children={game?.description_raw!} maxChars={300} />
-      <GameAttributes game={game!} />
+      <Heading mb={3}>{game.name}</Heading>
+      <TextExpandable children={game.description_raw} maxChars={300} />
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 }
